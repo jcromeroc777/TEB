541388f8229de3431fc4605123508a3d2de066de
Julio Romero <julio.romero@tutenlabs.com>
2024-09-29 11:50:32 -0400

    feat: auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDTO } from 'src/user/dto/user.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/v1/auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('signin')
+    async signIn(@Body() req: any) {
+        return await this.authService.signIn(req);
+    }
+
+    @Post('signup')
+    async signUp(@Body() userDTO: UserDTO) {
+        return await this.authService.signUp(userDTO);
+    }
+}
